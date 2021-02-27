bd6fd0f55496bca9ca51e7b59cec6bf231c29d25
AJ Auntor <auntorknocks@gmail.com>
2021-02-27 20:33:20 +0600

    React Recap Core concepts like Componants/Data pass/ Props etc

--- src/App.js
+++ src/App.js
@@ -2,24 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const productName = ["Apple Watch", "Ipad", "Cavit-C", "Computer Keyboard"]
   return (
     <div className="App">
+      <TvActor name={productName[1]} price="$20"></TvActor>
+      <TvActor name={productName[3]} price="$66"></TvActor>
+      <TvActor name={productName[0]} price="$13.64"></TvActor>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
       </header>
     </div>
   );
+}
+function TvActor(props) {
+  const style = {
+    border: "2px solid red",
+    margin: "10px",
+    padding: "10px",
+    borderRadius: "8px"
+  }
+  return (
+    <div style={style}>
+      <h1>This is a {props.name}</h1>
+      <p>This Product price is {props.price}. buy from Amazon</p>
+    </div>
+  )
+  
 }
 
 export default App;
